55b74f8d6b96894e08e2b31a3235c69131c2b197
partridgejiang <partridge@cpu.edu.cn>
2022-12-31 00:03:17 +0800

    Add a composer predefinedSetting selectbox

--- demo/src/components/composerAndViewer.js
+++ demo/src/components/composerAndViewer.js
@@ -19,14 +19,31 @@ class ComposerAndViewer extends React.Component {
 		this.composer = React.createRef();
 		this.viewer = React.createRef();
 
+		this.onPredefineSettingChange = this.onPredefineSettingChange.bind(this);
 		this.onComposerUserModificationDone = this.onComposerUserModificationDone.bind(this);
 	}
 	render()
 	{
 		return (<div className="ComposerAndViewerDemo">
-			<Composer className="SubWidget" ref={this.composer} predefinedSetting={this.state.composerPredefinedSetting} onUserModificationDone={this.onComposerUserModificationDone}></Composer>
-			<Viewer className="SubWidget" ref={this.viewer} predefinedSetting={this.state.viewerPredefinedSetting} chemObj={this.state.chemObj}></Viewer>
+			<div className="ControlPanel">
+				<label> Composer Predefined Setting:
+						<select value={this.state.composerPredefinedSetting} onChange={this.onPredefineSettingChange}>
+							<option value="fullFunc">fullFunc</option>
+							<option value="molOnly">molOnly</option>
+							<option value="compact">compact</option>
+						</select>
+				</label>
+			</div>
+			<div className="ComposerViewerPair">
+				<Composer className="SubWidget" ref={this.composer} predefinedSetting={this.state.composerPredefinedSetting} onUserModificationDone={this.onComposerUserModificationDone}></Composer>
+				<Viewer className="SubWidget" ref={this.viewer} predefinedSetting={this.state.viewerPredefinedSetting} chemObj={this.state.chemObj}></Viewer>
+			</div>
 		</div>);
+	}
+
+	onPredefineSettingChange(e)
+	{
+		this.setState({'composerPredefinedSetting': e.target.value});
 	}
 
 	onComposerUserModificationDone(e)
