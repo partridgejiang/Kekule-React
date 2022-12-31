fcbf77387ab08c88403afb4a978a174c4d330f09
partridgejiang <partridge@cpu.edu.cn>
2022-12-31 15:46:42 +0800

    Add info label to reflect to the selection of composer

--- demo/src/components/composerAndViewer.js
+++ demo/src/components/composerAndViewer.js
@@ -14,17 +14,33 @@ class ComposerAndViewer extends React.Component {
 		this.state = {
 			composerPredefinedSetting: 'molOnly',
 			viewerPredefinedSetting: 'basic',
-			chemObj: null
+			chemObj: null,
+			selectedObjs: undefined
 		};
 		this.composer = React.createRef();
 		this.viewer = React.createRef();
 
 		this.onPredefineSettingChange = this.onPredefineSettingChange.bind(this);
 		this.onComposerUserModificationDone = this.onComposerUserModificationDone.bind(this);
+		this.onComposerSelectionChange = this.onComposerSelectionChange.bind(this);
 	}
 	render()
 	{
+		let selectionInfoElem;
+		if (this.state.selectedObjs && this.state.selectedObjs.length)
+		{
+			let selDetails = this.getComposerSelectedAtomsAndBonds(this.state.selectedObjs);
+			selectionInfoElem = <span>You have selected { this.state.selectedObjs.length } object(s), including {selDetails.atoms.length} atom(s) and {selDetails.bonds.length} bond(s).</span>
+		}
+		else
+			selectionInfoElem = <span>Please edit and select objects in the composer to see the changes.</span>;
+
 		return (<div className="ComposerAndViewerDemo">
+			<div className="InfoPanel">
+				<label>
+					{ selectionInfoElem }
+				</label>
+			</div>
 			<div className="ControlPanel">
 				<label> Composer Predefined Setting:
 						<select value={this.state.composerPredefinedSetting} onChange={this.onPredefineSettingChange}>
@@ -35,10 +51,23 @@ class ComposerAndViewer extends React.Component {
 				</label>
 			</div>
 			<div className="ComposerViewerPair">
-				<Composer className="SubWidget" ref={this.composer} predefinedSetting={this.state.composerPredefinedSetting} onUserModificationDone={this.onComposerUserModificationDone}></Composer>
+				<Composer className="SubWidget" ref={this.composer} predefinedSetting={this.state.composerPredefinedSetting}
+				          onUserModificationDone={this.onComposerUserModificationDone} onSelectionChange={this.onComposerSelectionChange}></Composer>
 				<Viewer className="SubWidget" ref={this.viewer} predefinedSetting={this.state.viewerPredefinedSetting} chemObj={this.state.chemObj}></Viewer>
 			</div>
 		</div>);
+	}
+
+	getComposerSelectedAtomsAndBonds(selection)
+	{
+		let result = {atoms: [], bonds: []};
+		(selection || []).forEach(obj => {
+			if (obj instanceof Kekule.Atom)
+				result.atoms.push(obj);
+			else if (obj instanceof Kekule.Bond)
+				result.bonds.push(obj);
+		});
+		return result;
 	}
 
 	onPredefineSettingChange(e)
@@ -50,6 +79,10 @@ class ComposerAndViewer extends React.Component {
 	{
 		this.setState({chemObj: this.composer.current.getWidget().getChemObj()});
 		this.viewer.current.getWidget().requestRepaint();
+	}
+	onComposerSelectionChange(e)
+	{
+		this.setState({selectedObjs: this.composer.current.getWidget().getSelection()});
 	}
 }
 
